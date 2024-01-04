e258658d0e68da4185daba898e9d8bca4f11e348
muskankushwah5 <143391688+muskankushwah5@users.noreply.github.com>
2024-01-05 05:04:33 +0530

    Update ToDoAppComponent.vue

--- src/components/ToDoAppComponent.vue
+++ src/components/ToDoAppComponent.vue
@@ -1,98 +1,77 @@
 <template>
-    <div>
-      <h2>Todo List</h2>
-      <input v-model="newTask" placeholder="Enter a new task" />
-      <button class="styled-button" @click="addTask">Add Task</button>
-  
-      <!-- Incomplete Tasks Section -->
-      <section>
-        <h3>Incomplete Tasks</h3>
-        <div class="task-cards">
-          <div v-for="(task, index) in incompleteTasks" :key="index" class="task-card">
-            <div>
-              <input type="checkbox" v-model="task.done" class="styled-checkbox" @change="toggleTask(index)" />
-              <label :class="{ 'task-done': task.done }">{{ task.title }}</label>
-            </div>
-            <div v-if="editingIndex === -1">
-              <button class="styled-button" @click="editTask(index)">Edit</button>
-              <button class="styled-button" @click="deleteTask(index)">Delete</button>
-            </div>
-            <div v-if="editingIndex === index">
-              <input v-model="tasks[index].title" class="edit-input" />
-              <button class="styled-button" @click="saveTask(index)" :disabled="task.done">Save</button>
-              <button class="styled-button" @click="cancelEdit(index)">Cancel</button>
-            </div>
-          </div>
-        </div>
-      </section>
-  
-      <!-- Completed Tasks Section -->
-      <section>
-        <h3>Completed Tasks</h3>
-        <div class="task-cards">
-          <div v-for="(task, index) in completedTasks" :key="index" class="task-card">
+  <div>
+    <h2>Todo List</h2>
+    <input v-model="newTask" placeholder="Enter a new task" />
+    <button class="styled-button" @click="addTask">Add Task</button>
+
+    <!-- Incomplete Tasks Section -->
+    <section>
+      <h3>Incomplete Tasks</h3>
+      <div class="task-cards">
+        <div v-for="(task, index) in $store.incompleteTasks" :key="index" class="task-card">
+          <div>
+            <input type="checkbox" v-model="task.done" class="styled-checkbox" @change="toggleTask(index)" />
             <label :class="{ 'task-done': task.done }">{{ task.title }}</label>
+          </div>
+          <div v-if="$store.editingIndex === -1">
+            <button class="styled-button" @click="editTask(index)">Edit</button>
             <button class="styled-button" @click="deleteTask(index)">Delete</button>
           </div>
+          <div v-if="$store.editingIndex === index">
+            <input v-model="$store.tasks[index].title" class="edit-input" />
+            <button class="styled-button" @click="saveTask(index)" :disabled="task.done">Save</button>
+            <button class="styled-button" @click="cancelEdit(index)">Cancel</button>
+          </div>
         </div>
-      </section>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        newTask: '',
-        tasks: [],
-        editingIndex: -1,
-        originalTaskTitle: '',
-      };
+      </div>
+    </section>
+
+    <!-- Completed Tasks Section -->
+    <section>
+      <h3>Completed Tasks</h3>
+      <div class="task-cards">
+        <div v-for="(task, index) in $store.completedTasks" :key="index" class="task-card">
+          <label :class="{ 'task-done': task.done }">{{ task.title }}</label>
+          <button class="styled-button" @click="deleteTask(index)">Delete</button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { useTaskStore } from '../store/taskStore';
+
+export default {
+  data() {
+    return {
+      newTask: '',
+    };
+  },
+  methods: {
+    addTask() {
+      this.$store.addTask(this.newTask);
+      this.newTask = '';
     },
-    computed: {
-      incompleteTasks() {
-        return this.tasks.filter(task => !task.done);
-      },
-      completedTasks() {
-        return this.tasks.filter(task => task.done);
-      },
+    toggleTask(index) {
+      this.$store.toggleTask(index);
     },
-    methods: {
-      addTask() {
-        if (this.newTask.trim() === '') {
-          return;
-        }
-        this.tasks.push({ title: this.newTask, done: false });
-        this.newTask = '';
-      },
-      toggleTask(index) {
-        if (this.index === null) {
-          this.tasks[index].done = !this.tasks[index].done;
-        }
-      },
-      editTask(index) {
-        this.editingIndex = index;
-        this.originalTaskTitle = this.tasks[index].title;
-      },
-      saveTask(index) {
-        if (!this.tasks[index].done) {
-          if (this.tasks[index].title.trim() === '') {
-            this.tasks[index].title = this.originalTaskTitle;
-          }
-        }
-        this.editingIndex = -1;
-      },
-      cancelEdit(index) {
-        this.tasks[index].title = this.originalTaskTitle;
-        this.editingIndex = -1;
-      },
-      deleteTask(index) {
-        this.tasks.splice(index, 1);
-      },
+    editTask(index) {
+      this.$store.editTask(index);
     },
-  };
-  </script>
-  
+    saveTask(index) {
+      this.$store.saveTask(index);
+    },
+    cancelEdit(index) {
+      this.$store.cancelEdit(index);
+    },
+    deleteTask(index) {
+      this.$store.deleteTask(index);
+    },
+  },
+};
+</script>
+
   <style scoped>
 .task-done {
   text-decoration: line-through;

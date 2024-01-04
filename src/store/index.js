6fd3356ad6f40eaaffcebce0f9eafab35cc81cf7
muskankushwah5 <143391688+muskankushwah5@users.noreply.github.com>
2024-01-05 05:03:27 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,46 @@
-// src/store.js
+// src/store/taskStore.js
 import { defineStore } from 'pinia';
 
-export const useTodoStore = defineStore('todo', {
+export const useTaskStore = defineStore('task', {
   state: () => ({
     tasks: [],
+    editingIndex: -1,
+    originalTaskTitle: '',
   }),
+  getters: {
+    incompleteTasks: (state) => state.tasks.filter(task => !task.done),
+    completedTasks: (state) => state.tasks.filter(task => task.done),
+  },
   actions: {
-    addTask(task) {
-      this.tasks.push(task);
+    addTask(newTask) {
+      if (newTask.trim() === '') {
+        return;
+      }
+      this.tasks.push({ title: newTask, done: false });
     },
-    editTask(index, updatedTask) {
-      this.tasks.splice(index, 1, updatedTask);
+    toggleTask(index) {
+      if (this.index === null) {
+        this.tasks[index].done = !this.tasks[index].done;
+      }
+    },
+    editTask(index) {
+      this.editingIndex = index;
+      this.originalTaskTitle = this.tasks[index].title;
+    },
+    saveTask(index) {
+      if (!this.tasks[index].done) {
+        if (this.tasks[index].title.trim() === '') {
+          this.tasks[index].title = this.originalTaskTitle;
+        }
+      }
+      this.editingIndex = -1;
+    },
+    cancelEdit(index) {
+      this.tasks[index].title = this.originalTaskTitle;
+      this.editingIndex = -1;
     },
     deleteTask(index) {
       this.tasks.splice(index, 1);
-    },
-    toggleCompletion(index) {
-      this.tasks[index].done = !this.tasks[index].done;
     },
   },
 });
